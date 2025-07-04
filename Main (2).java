d329cea37ec288197e170b7b3f2bd01732af905f
Gobi-27project <gobikashreegs@gmail.com>
2025-07-04 21:54:04 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    int a=10;
+	   while(a>=0)
+	   {
+       System.out.println(a*3);
+       a--;
+	   }
+	}
+}
